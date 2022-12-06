f7fa7529a83902a56fdc302def47e482bfd03b7a
westxave <westxave@kkg-do.eu>
2022-12-06 11:17:37 +0100

    User

---
+++ src/main/java/my_project/model/User.java
@@ -0,0 +1,23 @@
+package my_project.model;
+
+public class User {
+
+    private String username;
+    private String id;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
